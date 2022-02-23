9c7b6764105e0ea23e5949e961cd3cf7da3d4ecd
Bhavya Tyagi <bhavyatyagi@Bhavyas-MacBook-Air.local>
2022-02-24 01:58:41 +0530

    update download url

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     # Provide either the link to your github or to your website
     url='https://github.com/bhavyatyagi/Topsis-Rank-Calulator.git',
     # I explain this later on
-    download_url='https://github.com/bhavyatyagi/Topsis-Rank-Calulator/archive/refs/tags/1.0.tar.gz',
+    download_url='https://github.com/bhavyatyagi/Topsis-Rank-Calulator/archive/refs/tags/1.1.tar.gz',
     # Keywords that define your package best
     keywords=['SOME', 'MEANINGFULL', 'KEYWORDS'],
     install_requires=[            # I get to this in a second
